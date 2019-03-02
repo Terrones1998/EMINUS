de6dde354d678357cce74d324114c20ad071ce77
Alvaro <alvateca@gmail.com>
2019-03-01 23:12:32 -0600

    Metodo Generico
    Actividad número 2 sobre implementación de un  método genérico dentro de una clase genérica

---
+++ Actividad_2_Tupla/src/mx/uv/facing/iinf/poo/Utilities.java
@@ -0,0 +1,7 @@
+package mx.uv.facing.iinf.poo;
+public class Utilities {
+    
+    public static <T,V> boolean Compare(T point1, V point2){
+            return point1.toString().equals(point2.toString());
+    }
+}
